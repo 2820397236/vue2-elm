c8508f1cfbee6c547c31c1cdbfcb34f5557a2233
lokisama <249722824@qq.com>
2017-09-01 18:15:25 +0800

    xx

--- src/config/env.js
+++ src/config/env.js
@@ -6,8 +6,8 @@
  * imgBaseUrl: 图片所在域名地址
  * 
  */
-// let baseUrl = 'http://vip.linkersocks.com:8890'; 
-let baseUrl = 'http://192.168.199.210:8889'; 
+let baseUrl = 'http://vip.linkersocks.com:8889'; 
+// let baseUrl = 'http://192.168.199.210:8889'; 
 // let baseUrl = 'http://127.0.0.1:8890'; 
 // let baseUrl = ''; 
 let routerMode = 'history';
