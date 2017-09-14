9ebfd4c1b918513e38c706bcde529335ced5276a
lokisama <249722824@qq.com>
2017-09-14 14:39:21 +0800

    xx

--- src/page/confirmOrder/confirmOrder.vue
+++ src/page/confirmOrder/confirmOrder.vue
@@ -409,14 +409,7 @@
                     alert(JSON.Stringify(res));
                  }
                  
-                 weixin.chooseWXPay({
-                    timestamp: json.timestamp, 
-                    nonceStr: json.nonceStr, // 支付签名随机串，不长于 32 位
-                    package:  json.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                    signType: json.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                    paySign: json.paySign, // 支付签名
-                    
-                });
+                 weixin.chooseWXPay(json);
 
                 // let user = JSON.parse(getStore('user'));
                 // addToCart(user.id,this.storeIds,this.payWayId).success(function(response){
