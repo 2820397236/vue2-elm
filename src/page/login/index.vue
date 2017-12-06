5f65246ce3afd7e1d34153bf376180222a0d3d7b
lokisama <249722824@qq.com>
2017-12-06 18:08:41 +0800

    x

--- src/page/login/index.vue
+++ src/page/login/index.vue
@@ -67,7 +67,7 @@
                         setStore('user',userRes.user);
 
                         if(userRes.user.username && userRes.user.username != ''){
-                            this.$router.push({path:'/home'});
+                            this.$router.push({path:'/analytics'});
                         }else{
                             this.$router.push({path:'/login'});
                         }
